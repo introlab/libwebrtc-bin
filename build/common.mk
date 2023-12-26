4b16ac336a7c8ef8a0be3847461a68188585927c
Dominic Létourneau <dominic.letourneau@usherbrooke.ca>
2023-12-26 05:57:29 -0500

    Copy boringssl and libyuv objects

--- build/common.mk
+++ build/common.mk
@@ -64,11 +64,15 @@ generate-licenses:
 common-copy: generate-licenses
 	rm -rf $(PACKAGE_DIR)/{lib,include,NOTICE,VERSION}
 	mkdir -p $(PACKAGE_DIR)/lib
+	mkdir -p $(PACKAGE_DIR)/lib/boringssl
+	mkdir -p $(PACKAGE_DIR)/lib/boringssl_asm
+	mkdir -p $(PACKAGE_DIR)/lib/libyuv_internal
 	mkdir -p $(PACKAGE_DIR)/include
 	cp $(BUILD_DIR)/obj/libwebrtc.a $(PACKAGE_DIR)/lib/libwebrtc.a
 	cp $(BUILD_DIR)/obj/third_party/boringssl/libboringssl.a $(PACKAGE_DIR)/lib/libboringssl.a
+	cp $(BUILD_DIR)/obj/third_party/boringssl/boringssl_asm/*.o $(PACKAGE_DIR)/lib/boringssl_asm
 	cp $(BUILD_DIR)/obj/third_party/libyuv/libyuv_internal.a $(PACKAGE_DIR)/lib/libyuv_internal.a
-
+	cp $(BUILD_DIR)/obj/third_party/libyuv/libyuv_internal/*.o $(PACKAGE_DIR)/lib/libyuv_internal
 	rsync -amv '--include=*/' '--include=*.h' '--include=*.hpp' '--exclude=*' $(SRC_DIR)/. $(PACKAGE_DIR)/include/.
 
 	cp -f $(BUILD_DIR)/LICENSE.md $(PACKAGE_DIR)/NOTICE
